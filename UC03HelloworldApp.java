7d749cab03ce181476b0f32921b5211a6a08258d
Amulya <amulyathimmareddy@gmail.com>
2026-03-22 12:45:31 +0530

    uc03 done

---
+++ UC03HelloworldApp.java
@@ -0,0 +1,14 @@
+
+class UC03HelloworldApp {
+   UC03HelloworldApp() {
+   }
+
+   public static void main(String[] var0) {
+      if (var0.length > 0) {
+         System.out.println("Hello " + var0[0]);
+      } else {
+         System.out.println("Hello Amulyareddy");
+      }
+
+   }
+}
