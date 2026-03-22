0611486d0d999596097ae505c2f4c60e298c9e21
Amulya <amulyathimmareddy@gmail.com>
2026-03-22 12:47:07 +0530

    uc03 done

---
+++ UC04HelloworldApp.java
@@ -0,0 +1,16 @@
+
+class UC04HelloworldApp {
+   UC04HelloworldApp() {
+   }
+
+   public static void main(String[] var0) {
+      if (var0.length == 0) {
+         System.out.println("Hello, World!");
+      } else {
+         for(String var4 : var0) {
+            System.out.println("Hello, " + var4 + "!");
+         }
+      }
+
+   }
+}
