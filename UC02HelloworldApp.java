6ca3226fef2bfdcc41e5fb4d2537d50afbef4ec7
Amulya <amulyathimmareddy@gmail.com>
2026-03-22 12:43:48 +0530

    uc02 done

---
+++ UC02HelloworldApp.java
@@ -0,0 +1,14 @@
+
+class UC02HelloworldApp {
+   UC02HelloworldApp() {
+   }
+
+   public static void main(String[] var0) {
+      if (var0.length > 0) {
+         System.out.println("Hello, " + var0[0] + "!");
+      } else {
+         System.out.println("Hello, World!");
+      }
+
+   }
+}
