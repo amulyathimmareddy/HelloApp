9fb28925c87feed644c4c4e6396f6ac9de12be70
Amulya <amulyathimmareddy@gmail.com>
2026-03-25 13:05:37 +0530

    uc06 done

---
+++ UC06HelloworldApp.java
@@ -0,0 +1,23 @@
+public class UC06HelloworldApp {
+
+    public static void main(String[] args) {
+        // If no arguments, print default greeting
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = "";
+
+            // Enhanced for loop to concatenate names
+            for (String name : args) {
+                names += name + ", ";
+            }
+
+            // Remove trailing comma and space using substring
+            names = names.substring(0, names.length() - 2);
+
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
+        }
+    
+}
+}
